4bfa41a166022defa08261cb0588ba014fef11b0
associacaoguaraunadev <seu-email@example.com>
2025-11-28 16:41:39 -0300

    fix: Corrigir padrão singleton do Prisma para serverless

--- api/lib/prisma.js
+++ api/lib/prisma.js
@@ -1,14 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma;
+const globalForPrisma = global;
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
