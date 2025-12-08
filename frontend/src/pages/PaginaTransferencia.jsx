83177a81bcf091eda38d384905e531c2b9849980
associacaoguaraunadev <seu-email@example.com>
2025-12-08 17:56:52 -0300

    refactor: change TransferenciaPessoas import to default for consistency

--- frontend/src/pages/PaginaTransferencia.jsx
+++ frontend/src/pages/PaginaTransferencia.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAuth } from '../contexto/AuthContext';
 import { Navigate } from 'react-router-dom';
-import { TransferenciaPessoas } from '../componentes/TransferenciaPessoas';
+import TransferenciaPessoas from '../componentes/TransferenciaPessoas';
 import Breadcrumb from '../componentes/Breadcrumb';
 import './PaginaTransferencia.css';
 
